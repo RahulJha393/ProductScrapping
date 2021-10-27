9b72d13f4a31a3b4ba2fc8f71e3f70c26900d087
Rahul Kumar Jha <67184791+RahulJha393@users.noreply.github.com>
2021-10-27 10:13:03 +0530

    Product scrapping
    Scrapping product from amazon and flipkart

---
+++ costlyItem-Rahul.py
@@ -0,0 +1,186 @@
+'''
+                                                       Costly Item (Problem Statement)
+
+While shopping online it can be seen that there are different price tag for the same item on different websites.
+Help people to find the same product in less price to save money.
+
+'''
+
+#  Importing module    
+#--------------------------------------------------------
+
+from tkinter import *
+from tkinter import Scrollbar
+from bs4 import BeautifulSoup
+import requests
+import webbrowser
+import pyttsx3
+
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
+flipkart=''
+amazon=''
+
+###########################################################################
+#                   Important function                                                                                                 #
+###########################################################################
+
+def say(s):
+    engine = pyttsx3.init()
+    engine.say(s)
+    engine.setProperty("rate",178)
+    engine.runAndWait()
+
+
+
+# use to find most relevant items (Optimising the result)
+def rele(main,search):
+    relation = 0
+    main = main.split()
+    search = search.split()
+    total =sum([x+1 for x in range(len(search))])
+    for x in range(len(search)):
+        if(search[x] in main):
+            relation += (len(search)-x)/total
+            main.remove(search[x])
+    return relation
+# use to convert currency
+def convert(a):
+    return int(float(a.replace(" ",'').replace("INR",'').replace(",",'').replace("₹",'')))
+
+# use to extract product from flipkart
+def flipkart(name = ""):
+    try:
+        global flipkart_url
+        name1 = name.replace(" ","+")
+        name=name.upper()#iphone x  -> iphone+x
+        flipkart_url=f'https://www.flipkart.com/search?q={name1}&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=off&as=off'
+        res = requests.get(f'https://www.flipkart.com/search?q={name1}&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=off&as=off',headers=headers)
+
+        soup = BeautifulSoup(res.text,'html.parser')
+        score=0
+        for x in range(len(soup.select('._4rR01T'))):
+            flipkart_name=soup.select('._4rR01T')[x].getText().strip().upper()
+            tempScore = rele(flipkart_name,name)
+            if tempScore>score:
+                score = tempScore
+                flipkart_price = soup.select('._1_WHN1')[0].getText().strip()
+            if(score==1):
+                return f"{flipkart_name}\nPrise : {flipkart_price}\n"
+        if(score==0):
+            return '           Product Not Found'
+    except :
+        return "           Some Error"
+    return f"{flipkart_name}\nPrise : {flipkart_price}\n"
+# use to extract product from amazon
+def amazon(name):
+    try:
+        global amazon_url
+        name1 = name.replace(" ","-")
+        name2 = name.replace(" ","+")
+        amazon_url=f'https://www.amazon.in/{name1}/s?k={name2}'
+        #res = requests.get(f'https://www.amazon.in/{name1}/s?k={name2}',headers=headers)
+        res = requests.get(f'https://www.amazon.in/s?k={name}',headers=headers)
+        print(type(res))
+        soup = BeautifulSoup(res.text,'html.parser')
+        amazon_page = soup.select('.a-color-base.a-text-normal')
+        amazon_page_length = int(len(amazon_page))
+        score=0
+        for i in range(0,amazon_page_length):
+            name = name.upper()
+            amazon_name = soup.select('.a-color-base.a-text-normal')[i].getText().strip().upper()
+            tempScore = rele(amazon_name,name)
+            print(amazon_name,"+",name,score)
+            if tempScore>score:
+                score = tempScore
+                amazon_name = soup.select('.a-color-base.a-text-normal')[i].getText().strip().upper()
+                try:
+                    amazon_price = soup.select('.a-price-whole')[i].getText().strip().upper()
+                except:
+                    amazon_price = "Price not given"
+                if(score==1):
+                    break
+                #break
+            else:
+                i+=1
+                i=int(i)
+                if i==amazon_page_length and score==0:
+                    return '           Product Not Found1'
+                    break
+        return f"{amazon_name}\nPrice : {amazon_price}\n"
+    except :
+        amazon_price = '           Some Error Occured'
+    return amazon_price
+
+def urls():
+    return f"{flipkart_url}\n\n\n{amazon_url}"
+
+def open_url(event):
+        global flipkart
+        global amazon
+        webbrowser.open_new(flipkart)
+        webbrowser.open_new(amazon)
+
+def search():
+    
+    box1.delete(1.0,"end")
+    box4.delete(1.0,"end")
+    box6.delete(1.0,"end")
+    if(product_name_entry.get().split()!=[]):
+        t1=flipkart(product_name_entry.get())
+
+        box1.insert(1.0,t1)
+
+        t4=amazon(product_name_entry.get())
+        box4.insert(1.0,t4)
+
+        t6 = urls()
+        box6.insert(1.0,t6)
+    else:
+        box6.insert(1.0,'Please enter name of the product');
+###########################################################################
+#                   Creating GUI                                                                                                         #
+###########################################################################
+window = Tk()
+window.wm_title("Web Scrapping Portal | Rahul")
+window.minsize(1500,700)
+window.config(background='#49A')
+# lable
+lable_one =  Label(window,relief=GROOVE, text="Enter Product Name ", font=("courier", 15))
+lable_one.place(relx=0.2, rely=0.1, anchor="center")
+# product name 
+product_name =  StringVar()
+product_name_entry =  Entry(window, textvariable=product_name, width=70)
+product_name_entry.place(relx=0.5, rely=0.1, anchor="center")
+# search Button
+search_button =  Button(window,relief=GROOVE, text="Search", width=15, font=("courier", 15), command= search)
+search_button.place(relx=0.5, rely=0.2, anchor="center")
+
+# creating Label of boxes
+flipkartLabel =  Label(window, relief=GROOVE,text="Flipkart", font=("courier", 15),bg='#49A')
+amazonLabel =  Label(window,relief=GROOVE, text="Amazon", font=("courier", 15),bg='#49A')
+resultLabel =  Label(window,relief=GROOVE, text="All URLs", font=("courier", 15),bg='#49A')
+l8 =  Label(window, text="Loding.....", font=("courier", 30))
+
+flipkartLabel.place(x=100,y=240)
+amazonLabel.place(x=555,y=240);
+#resultLabel.place(relx=0.8, rely=0.6, anchor="center")
+resultLabel.place(x=1000,y=240)
+
+# inserting scrollbar in boxes
+scrollbar = Scrollbar(window)
+box1 =  Text(window ,bd=7,relief=SUNKEN,height=15, width=50, yscrollcommand=scrollbar.set)
+box4 =  Text(window,bd=7,relief=SUNKEN, height=15, width=50, yscrollcommand=scrollbar.set)
+
+box1.place(x=100,y=310)
+box4.place(x=550,y=310)
+
+box6 =  Text(window,bd=7,relief=SUNKEN, height=15, width=50, yscrollcommand=scrollbar.set, fg="blue", cursor="hand2")
+#box6.place(relx=0.8, rely=0.8, anchor="center")
+box6.place(x=1000,y=310)
+box6.bind("<Button-1>", open_url)
+say("Welcome to Web scrapping portal")
+window.mainloop()
+
+
---
+++ readme.md
@@ -0,0 +1,41 @@
+ # Costly Item (Problem Statement)
+
+While shopping online it can be seen that there are different price tag for the same item on different websites.
+Help people to find the same product in less price to save money.
+
+## Solution Proposed
+
+As per the problem's discription it's little pathetic for the people to search for the same product over different websites to compare, so to make this task easier this application scrap the product's price and details and bring it alltogether. 
+
+## Technology Used
+
+### Language Used
+Python version : 3.9.6
+
+### Pre installed Dependencies
+* tkinter
+* Webbrowser
+
+### Dependencies
+* BeautifulSoup :version 4.9.3
+* requests :2.26.0
+* pyttsx3 : version 2.90
+
+## How to Use
+# 
+Run in Command prompt 
+
+Press window( ⊞) + R and type cmd  
+Once the cmd is up and running run the following code
+```
+       python costlyItem-Rahul.py
+```
+Now in the search box write the name of the product you wanna search and press 'search' to scrap the details of the product you want.  
+
+Note: This project is currently oprimized for electronic products 
+
+Once you search the product the first most relevent searched product will be displayed in the panel below also you will get the link to the webpage where the product is displayed.
+
+
+Name :  Rahul Kumar Jha
+Class :  0177CS1129
